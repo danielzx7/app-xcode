e5a4fbec540c23bf829a730017f4e067a52e8d93
danielzx7 <danielkim2020@u.northwestern.edu>
2018-02-15 16:16:05 -0600

    initial commit

---
+++ Project Mark Jung/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Project Mark Jung
+//
+//  Created by Daniel Kim on 1/23/18.
+//  Copyright © 2018 Daniel Kim. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    @IBAction func LoginPressed(_ sender: UIButton) {
+    }
+    
+}
+
